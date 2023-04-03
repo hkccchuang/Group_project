72e7e5b241b9d0d18a1502edd56875b0f106b3c6
hkccchuang <s919588773@gmail.com>
2023-04-03 23:01:24 +0800

    initial commit

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -2,16 +2,72 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static MediaPlayer mMediaPlayer = new MediaPlayer();//BGM
+     public Intent next;
+     ImageButton start;
+
+     Boolean isNext=false;//If user go to other intent,music will not be pause
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();//hide the action bar
+        loadMusic();//bgm
+        loadUI();
+
+    }
+
+    public void loadMusic(){
+        mMediaPlayer = MediaPlayer.create(this, R.raw.bgm);
+        mMediaPlayer.setLooping(true);
+        mMediaPlayer.start();
+    }
+
+    public void loadUI(){
+        start=findViewById(R.id.btnStart);
+        start.setOnClickListener(this);
+    }
+
+
+    protected void onPause(){
+        super.onPause();
+
+        if(!isNext){
+        mMediaPlayer.pause();
+        //If user go to other intent,music will not be pause
+        }
+
+
+
+    }
+
+    protected void onResume(){
+        super.onResume();
+        mMediaPlayer.start();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId()==R.id.btnStart){
+            next=new Intent(MainActivity.this, StageSelection.class);
+            isNext=true;
+            startActivity(next);
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/groupproject/StageSelection.java
@@ -0,0 +1,15 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StageSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage_selection);
+        MainActivity.mMediaPlayer.start();
+    }
+}
